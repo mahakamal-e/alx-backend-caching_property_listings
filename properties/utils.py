530e38c97ae3f16e99123ce9938bed45f8abdaf9
mahakamal-e <mahakamal09@gmail.com>
2025-09-17 18:43:43 +0300

    edit

--- properties/utils.py
+++ properties/utils.py
@@ -9,9 +9,6 @@ def get_all_properties():
     """
     properties = cache.get("all_properties")
     if properties is None:
-        
-        properties = list(
-            Property.objects.values("id", "title", "description", "price")
-        )
+        properties = list(Property.objects.all())
         cache.set("all_properties", properties, 3600)
     return properties
