d59fbd1ab368647abf055687e3034decf9945683
mahakamal-e <mahakamal09@gmail.com>
2025-09-17 18:46:29 +0300

    add signals

--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+
+    def ready(self):
+        # Import signals so they get registered
+        import properties.signals
