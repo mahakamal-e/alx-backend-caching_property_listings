1e52422c16014fff4710b7c092303ca2229934c6
mahakamal-e <mahakamal09@gmail.com>
2025-09-17 18:39:27 +0300

    utils

--- properties/utils.py
+++ properties/utils.py
@@ -1,13 +1,17 @@
 from django.core.cache import cache
 from .models import Property
 
+
 def get_all_properties():
     """
-    Retrieve all Property objects.
-    Cache them in Redis for 1 hour (3600 sec).
+    Fetch all properties from cache if available,
+    otherwise query the database and store in cache for 1 hour.
     """
     properties = cache.get("all_properties")
     if properties is None:
-        properties = list(Property.objects.values("id", "title", "description", "price"))
+        
+        properties = list(
+            Property.objects.values("id", "title", "description", "price")
+        )
         cache.set("all_properties", properties, 3600)
     return properties
